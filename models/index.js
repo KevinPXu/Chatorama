992b0393adb1818506cff115fb5d02aee94259ac
Kevin Xu <kevinpxu21@gmail.com>
2022-11-09 14:45:42 -0600

    updated index.js for model relationships to include chatroom relationships

--- models/index.js
+++ models/index.js
@@ -1,26 +1,27 @@
-const User = require('./User');
-const Message = require('./Message');
-const Chatroom = require('./Chatroom');
-const UserChat = require('./UserChat');
+const User = require("./User");
+const Message = require("./Message");
+const Chatroom = require("./Chatroom");
+const UserChat = require("./UserChat");
 
 //user-message
 Message.belongsTo(User, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 //user-chatroom (thru UserChat)
+Chatroom.belongsToMany(User, { through: UserChat, foreignKey: "chatroom_id" });
 
+User.belongsToMany(Chatroom, { through: UserChat, foreignKey: "user_id" });
 
+Chatroom.hasMany(Message, foreignKey: 'chatroom_id');
 //message-chatroom
 Message.belongsTo(User, {
-    foreignKey: 'chatroom_id'
+  foreignKey: "chatroom_id",
 });
 
-
-
 module.exports = {
-    User,
-    Message,
-    Chatroom,
-    UserChat
-}
+  User,
+  Message,
+  Chatroom,
+  UserChat,
+};
