e77fdfa3bcc623e2baf902d66b4142ae6940425f
Angellyn218 <angellyncervantes@yahoo.com>
2022-11-10 13:56:35 -0800

    Added homeRoutes to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 // include page routes in here at some points
+const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
