f29ab519a7204f5982068fbffbe56d4bb209dcb6
[carlos martinez] <theycallhellp@gmail.com>
2022-11-13 13:37:18 -0800

    invalid date changed to no current messages

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,6 +2,9 @@ module.exports = {
   format_date_time: (date) => {
     // Format date as MM/DD/YYYY
     const postDate = new Date(date);
+    if(postDate == 'Invalid Date') {
+      return 'No Current Messages';
+    }
     return postDate.toLocaleString();
   },
 };
